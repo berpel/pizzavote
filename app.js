88e37c00def559725e42509f633c9f977716070e
Kaveh Nowroozi <swervogm@gmail.com>
2013-10-25 14:05:34 -0400

    change topping objects

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ var express = require('express')
   , mongoose = require('mongoose')
   , stylus = require('stylus')
   , nib = require('nib')
-  , randomstring = require("randomstring");
+  , randomstring = require("randomstring")
+  , votes = require("./models/votes");
 
 var db = mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost/pizzavote");
-
 mongoose.connection.db.dropDatabase();
 
 var app = express();
 var port = process.env.PORT || 3000;
+
+var toppings = {};
+toppings.meats = [{"n":"Ham","c":"ham"},{"n":"Beef","c":"beef"},{"n":"Pepperoni","c":"pepperoni"},{"n":"Sausage","c":"sausage"},{"n":"Bacon","c":"bacon"},{"n":"Chicken","c":"chicken"}];
+toppings.vegetables = [{"n":"Onions","c":"onions"},{"n":"Green Peppers","c":"greenpeppers"},{"n":"Black Olives","c":"blackolives"},{"n":"Mushrooms","c":"mushrooms"},{"n":"Tomatoes","c":"tomatoes"}];
+toppings.specialty = [{"n":"Meat Lovers","c":"meatlovers","toppings":['ham','beef','pepperoni','sausage']},{"n":"Veggie","c":"veggie","toppings":['blackolives','onions','greenpeppers','mushrooms','tomatoes']},{"n":"Supreme","c":"supreme","toppings":['blackolives','onions','greenpeppers','mushrooms','tomatoes','ham','beef','pepperoni','sausage']}];
 
 function compile(str, path) {
   return stylus(str)
@@ -23,61 +28,30 @@ function compile(str, path) {
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
-app.use(stylus.middleware(
-  { src: __dirname + '/public'
-  , compile: compile
-  }
-));
+app.use(stylus.middleware({ 
+	src: __dirname + '/public', 
+	compile: compile
+  })
+);
 app.use(express.static(__dirname + '/public'));
-
-
-var Toppings = require("./models/toppings");
-var Votes = require("./models/votes");
-
-// don't know how to migrate shit
-Toppings.remove(function(err, p){
-    if(err){ 
-        throw err;
-    } else{
-        console.log('No Of Documents deleted:' + p);
-    }
-});
-
-
-// insert some data
-new Toppings({
-    name: 'Pepperoni',
-    type: 'meat'
-  }).save();
-
-new Toppings({
-    name: 'Sausage',
-    type: 'meat'
-  }).save();
-
-new Toppings({
-    name: 'Ham',
-    type: 'meat'
-  }).save();
 
 // routes
 app.get('/', function(req, res) {
-  Toppings.find(function(err, toppings) {
     console.log(toppings);
     res.render('index',
     { title : 'Home',
       toppings : toppings }
     );
-  });
 });
 
 app.post('/', function(req, res) {
   console.log('post');
+  console.log(req.params);
 });
 
 app.get('/generate', function(req, res) {
   var key = randomstring.generate(7);
-  new Votes({
+  new votes({
     key: key
   });
   console.log(key);
@@ -85,14 +59,12 @@ app.get('/generate', function(req, res) {
 });
 
 app.get('/:key', function(req, res) {
-  var query = Votes.findOne({'key': req.params.id});
+  var query = votes.findOne({'key': req.params.id});
   console.log(query);
-  Toppings.find(function(err, toppings) {
     res.render('vote',
     { title : 'Create',
       toppings : toppings }
     );
-  });
 });
 
 app.listen(port, function() {
